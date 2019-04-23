d7418b0df1847c90ed39f7e6fda9513722b3dc60
Echostt <31448222+Echostt@users.noreply.github.com>
2019-04-23 14:16:57 -0600

    Motion Update
    Updates to the movement of each satellite, no longer based on angles, but travels around a curve from a fixed point.

--- Kessler/Assets/Scripts/Kessler/clsSatellite.cs
+++ Kessler/Assets/Scripts/Kessler/clsSatellite.cs
@@ -1,14 +1,32 @@
 ﻿using UnityEngine;
 
-public class clsSatellite : MonoBehaviour {
-	public float posTheta = 0,  posPhi = 0,
-				thetaRate = 0,  phiRate = 0, 
-				dist = 0, angleRate = 0;
-	public int collisionsRemaining;
+public class clsSatellite : MonoBehaviour
+{
+    public float posTheta = 0, posPhi = 0,
+                thetaRate = 0, phiRate = 0,
+                dist = 0, angleRate = 0;
+    public int collisionsRemaining;
     private const float cPI = Mathf.PI;
 
-	//move object based on fixed angular rate around a sphere
-	public void angleMove(){
+    public float randX, randY, randZ;
+
+    void Start()
+    {
+        Random.InitState(System.DateTime.Now.Millisecond *
+            (int)this.gameObject.transform.position.x *
+            (int)this.gameObject.transform.position.y *
+            (int)this.gameObject.transform.position.z);
+        randX = Random.value * 360;
+        if ((int)randX % 2 == 0) randX *= -1;
+        randY = Random.value * 360;
+        if ((int)randY % 2 == 0) randY *= -1;
+        randZ = Random.value * 360;
+        if ((int)randZ % 2 == 0) randZ *= -1;
+    }
+
+    //move object based on fixed angular rate around a sphere
+    public void angleMove()
+    {
         //reduction in angle movement to elim overflow after long idle
         if (posPhi >= 2 * cPI)
             posPhi -= 2 * cPI;
@@ -20,6 +38,7 @@ public class clsSatellite : MonoBehaviour {
             posTheta += 2 * cPI;
 
         // Add offsets to posPhi and posTheta for diff arrangements //TD ////////////////////
+        /*
         this.posPhi += (angleRate * phiRate);
         this.posTheta += (angleRate * thetaRate);
 		float sinTheta = Mathf.Sin(posTheta);
@@ -28,27 +47,33 @@ public class clsSatellite : MonoBehaviour {
 			dist * sinTheta * Mathf.Sin(posPhi),
 			dist * Mathf.Cos(posTheta))
 		);
-        
-        //this.gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 100 * Time.deltaTime);
-	}
-		
-    
-	public void OnTriggerEnter(Collider col){
-		this.collisionsRemaining -= 1;
-		if(this.collisionsRemaining > 0) {
-			if(col.gameObject.tag == "Debris") {
-				this.GetComponent<Renderer>().material.color = new Vector4(0, 0, 1.0f, 0); //blue
-			} else {
-				this.GetComponent<Renderer>().material.color = new Vector4(1.0f, 0, 0, 0); //red
-			}
-			//remove collision detection 
-			this.GetComponent<Rigidbody>().detectCollisions = false;
+        */
+        //this.gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 10 * Time.deltaTime);
+        this.gameObject.transform.RotateAround(Vector3.zero, new Vector3(randX, randY, randZ), angleRate);
+    }
+
+
+    public void OnTriggerEnter(Collider col)
+    {
+        this.collisionsRemaining -= 1;
+        if (this.collisionsRemaining > 0)
+        {
+            if (col.gameObject.tag == "Debris")
+            {
+                this.GetComponent<Renderer>().material.color = new Vector4(0, 0, 1.0f, 0); //blue
+            }
+            else
+            {
+                this.GetComponent<Renderer>().material.color = new Vector4(1.0f, 0, 0, 0); //red
+            }
+            //remove collision detection 
+            this.GetComponent<Rigidbody>().detectCollisions = false;
             //pass current gameobject to create debris from position/orbit
-			GameObject.FindWithTag("GameController").GetComponent<SatManager>().createDebris(this.gameObject);
-			this.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-			//stop collided objects from moving
-			angleRate = 0;
-		}
-	}
-    
+            GameObject.FindWithTag("GameController").GetComponent<SatManager>().createDebris(this.gameObject);
+            this.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            //stop collided objects from moving
+            angleRate = 0;
+        }
+    }
+
 }
